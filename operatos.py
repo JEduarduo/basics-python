a6c4511617273fc6131ee881da2ab20b28c1e795
JEduarduo <eddu0619@hotmail.com>
2023-05-03 11:49:20 -0600

    first commit

---
+++ operatos.py
@@ -0,0 +1,5 @@
+
+addition = 10 + 5 #resultado 15
+substracion = 10 - 5 #resultado 5
+multiplication = 10 * 5 #resultado 50
+
---
+++ variables.py
@@ -0,0 +1,10 @@
+
+name = "Eduardo"
+last_name = "Espinoza"
+age = 22
+list_shopping = ("milk", "eggs", "vegetables")
+
+
+print(f"{name} {last_name} {age}")
+print(name + " " +last_name + "" +git branchage)
+print(list_shopping)
