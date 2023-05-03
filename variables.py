7aa16add720caa123964b2b250cd767c57d04285
JEduarduo <eddu0619@hotmail.com>
2023-05-03 12:34:33 -0600

    ADD: Corrrecciones

---
+++ variables.py
@@ -0,0 +1,11 @@
+
+name = "Eduardo"
+last_name = "Espinoza"
+age = 22
+list_shopping = ("Milk", "Eggs", "Vegetables")
+
+print(f"{name} {last_name} {age}")
+
+
+print(name + " " + last_name + " " + str(age))
+print(list_shopping)
